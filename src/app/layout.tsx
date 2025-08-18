b7f1e10f92857b16a5b532b63d192e61b8f45a4b
Jaimin Raval <163107078+Jaimin7364@users.noreply.github.com>
2025-08-18 19:30:28 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Jaimin Raval',
-  description: 'I am a professional Computer Science Engineer, Software Engineer, Developer, App Developer, Cloud Enthusiast, and DevOps Expert. Specializing in building scalable applications, cloud solutions, and modern DevOps practices.',
+  description:
+    'I am a professional Computer Science Engineer, Software Engineer, Developer, App Developer, Cloud Enthusiast, and DevOps Expert. Specializing in building scalable applications, cloud solutions, and modern DevOps practices.',
   icons: {
     icon: '/assets/logo.png',
   },
@@ -20,9 +21,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Person",
+              "name": "Jaimin Raval",
+              "url": "https://www.jaiminraval.dev",
+              "image": "https://www.jaiminraval.dev/assets/logo.png",
+              "jobTitle": "Software Engineer",
+              "sameAs": [
+                "https://github.com/Jaimin7364",
+                "www.linkedin.com/in/jaimin-raval-132751283",
+                "https://x.com/JaiminRaval100?t=ZVjx3H-CihfeafrtLlg0ZA&s=09"
+              ]
+            }),
+          }}
+        />
+      </head>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
