ce4d57f1c7d214d1bc65e45807bd8297816f0ddd
Jaimin7364 <jaiminraval100@gmail.com>
2026-07-11 11:10:03 +0530

    google ads added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -114,6 +114,7 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+        <meta name="google-adsense-account" content="ca-pub-8873359884266322" />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8873359884266322"
